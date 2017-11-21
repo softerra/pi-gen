9ea54a29651a9dc23c76a8a244faa47867c33935
Alexander Dsugan <alex@softerra.com>
2017-11-21 17:12:50 +0200

    Re-ordered iotc-core and iotc-ide installation steps

--- stage2.1/01-install-iotcrafter/01-run.sh
+++ stage2.1/01-install-iotcrafter/01-run.sh
@@ -6,13 +6,14 @@ on_chroot << EOF
 uname -a # check cpu
 
 apt-get -y update
+
 echo iotc iotc/cpuid string RPI | debconf-set-selections
-echo iotc iotc/load-overlays boolean false | debconf-set-selections
 #echo iotc iotc/kernvers string {kern_version} | debconf-set-selections
+echo iotc iotc/load-overlays boolean false | debconf-set-selections
+
 apt-get -y --allow-unauthenticated install iotc-core iotc-ide
+dpkg-reconfigure -fnoninteractive -plow unattended-upgrades
 
 # remove all iotc's settings
 echo PURGE | debconf-communicate iotc
-
-dpkg-reconfigure -fnoninteractive -plow unattended-upgrades
 EOF
