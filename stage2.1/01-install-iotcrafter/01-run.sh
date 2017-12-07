f90e3fd3330fc3fa155c63f4edb61d5f211077db
Alexander Dsugan <alex@softerra.com>
2017-12-07 20:14:38 +0200

    Modified iotcrafter image uild for using of signed repo

--- stage2.1/01-install-iotcrafter/01-run.sh
+++ stage2.1/01-install-iotcrafter/01-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 install -m 644 files/iotcrafter.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
+wget -qO - http://iotcrafter.com:8888/iotc/iotcrafter.gpg.key | apt-key add -
 
 on_chroot << EOF
 uname -a # check cpu
@@ -11,7 +12,7 @@ echo iotc iotc/cpuid string RPI | debconf-set-selections
 #echo iotc iotc/kernvers string {kern_version} | debconf-set-selections
 echo iotc iotc/load-overlays boolean false | debconf-set-selections
 
-apt-get -y --allow-unauthenticated install iotc-core iotc-ide
+apt-get -y install iotc-core iotc-ide
 dpkg-reconfigure -fnoninteractive -plow unattended-upgrades
 
 # remove all iotc's settings
