2482280607336854a9d8b49aec7a09d0949100c0
Alexander Dsugan <alex@softerra.com>
2018-05-07 20:01:40 +0300

    Updated iotc_init.sh (cleanup of iotc data after it is saved and applied)

--- stage2.1/01-install-iotcrafter/02-run.sh
+++ stage2.1/01-install-iotcrafter/02-run.sh
@@ -2,7 +2,7 @@
 
 # install init script
 # ver.1
-IOTC_INIT_REV=190100147f40a4b3495f5808cd0c1d44434d4874
+IOTC_INIT_REV=b8b4e41ada14892ee2f90a0419b14cb159cfe4cf
 wget -P ${ROOTFS_DIR}/opt/iotc/bin/ https://raw.githubusercontent.com/softerra/iotc_scripts/${IOTC_INIT_REV}/board/iotc_init.sh
 sed -i 's/^\(iotc_init_version=\).*$/\1"'${IOTC_INIT_REV}'"/' ${ROOTFS_DIR}/opt/iotc/bin/iotc_init.sh
 chmod 755 ${ROOTFS_DIR}/opt/iotc/bin/iotc_init.sh
