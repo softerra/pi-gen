af33fd6aeec0c2b81b1a09fa97fdcc899f70f7f7
Alexander Dsugan <alex@softerra.com>
2018-08-14 23:19:28 +0300

    added iotc-version into the image

--- stage2.1/01-install-iotcrafter/01-run.sh
+++ stage2.1/01-install-iotcrafter/01-run.sh
@@ -2,6 +2,13 @@
 
 install -m 644 files/iotcrafter.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
 
+VERFILE=${ROOTFS_DIR}/opt/iotc/etc/version.json
+install -d -m 755 $(dirname $VERFILE)
+install -m 644 files/version.json $(dirname $VERFILE)
+. ${BASE_DIR}/iotcrafter/iotc-version
+sed -i -e 's/"image"[^"]*"[^"]*\(".*\)$/"image": "'$IOTC_VERSION'\1/' $VERFILE
+sed -i -e 's/"image-build"[^"]*"[^"]*\(".*\)$/"image-build": "'$IMG_NAME'\1/' $VERFILE
+
 on_chroot << EOF
 set -e
 
