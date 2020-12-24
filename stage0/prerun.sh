1e57e93f7b67d872c82893b2474582d719dff1b5
Alexander Dsugan <alex@softerra.com>
2020-12-24 13:46:11 +0200

    try bootstrap from a specific mirror (to avoid failures with local fastmirror.pp.ua)

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+#MIRROR=http://raspbian.raspberrypi.org/raspbian/
+MIRROR=http://debian.bio.lmu.de/raspbian/raspbian/
+#MIRROR=http://mirror.netzwerge.de/raspbian/
+
 if [ ! -d "${ROOTFS_DIR}" ]; then
-	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
+	bootstrap ${RELEASE} "${ROOTFS_DIR}" ${MIRROR}
 fi
